16173541797a2e7e8d2e3f88bf0f24b7eeece4c9
tohsaka888 <156132264@qq.com>
2022-11-14 11:58:39 +0800

    🌈 style: style

--- source/index.styled.ts
+++ source/index.styled.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { ModeProps } from "./type";
 
 const staticImage = `https://s2.music.126.net/style/web2/img/frame/playbar.png?d1dfc51fd30ea63831b89963a00ef004`;
 const sliderImage = `https://s2.music.126.net/style/web2/img/frame/statbar.png?b9ea7630bac1c54faa4ae5ae30fb21b5`;
 const iconImage = `https://s2.music.126.net/style/web2/img/iconall.png?066b1a7cd07d2ca9d13f4f506cc7f3e3`;
+const PipIconImage = `https://p1.music.126.net/DLVi_1eymwAX8gDunfd2bg==/109951165524394991.png`;
 
 export const MusicPlayerContainer = styled.div`
   width: 980px;
@@ -34,9 +36,10 @@ export const ActiveArea = styled.div`
   z-index: -1;
 `;
 
-export const LockedIcon = styled.div`
+export const LockedIcon = styled.div<{ isLocked: boolean }>`
   background: ${() => `url(${staticImage})`};
-  background-position: -100px -400px;
+  background-position: ${({ isLocked }) =>
+    isLocked ? "-100px -400px" : "-80px -400px"};
   display: block;
   width: 18px;
   height: 18px;
@@ -44,6 +47,7 @@ export const LockedIcon = styled.div`
   position: absolute;
   right: calc(50% - 9px);
   bottom: calc(50% + 10px);
+  z-index: 10;
 `;
 
 export const LeftContainer = styled.div`
@@ -63,7 +67,7 @@ export const RightBackground = styled.div`
   top: -1px;
   right: 0;
   width: 15px;
-  height: 53px;
+  height: 58px;
   background-position: -52px -393px;
   pointer-events: none;
   z-index: -1;
@@ -205,5 +209,105 @@ export const TimeArea = styled.div`
   align-items: center;
   &:first-child {
     color: #a1a1a1;
+  }
+`;
+
+export const PictureInPictureIcon = styled.div`
+  /* margin-top: 1px; */
+  background: ${() => `url(${PipIconImage}) no-repeat 0 0;`};
+  width: 25px;
+  height: 25px;
+  margin-left: 24px;
+  &:hover {
+    background-position-y: -25px;
+  }
+`;
+
+export const CollectIcon = styled.div`
+  margin-top: 1px;
+  background: ${() => `url(${staticImage}) no-repeat 0 9999px;`};
+  width: 25px;
+  height: 25px;
+  background-position: -88px -163px;
+  &:hover {
+    background-position: -88px -189px;
+  }
+`;
+
+export const ShareIcon = styled.div`
+  margin-top: 1px;
+  width: 25px;
+  height: 25px;
+  background: ${() => `url(${staticImage}) no-repeat 0 9999px;`};
+  background-position: -114px -163px;
+  &:hover {
+    background-position: -114px -189px;
+  }
+`;
+
+export const SliderIcon = styled.div`
+  background: ${() => `url(${staticImage})`};
+  width: 10px;
+  background-position: -147px -238px;
+  z-index: 10;
+  height: 46px;
+  margin: 0px 8px;
+  margin-right: 2px;
+`;
+
+export const VolumeIcon = styled.div<{ isMuted: boolean }>`
+  margin-top: 2px;
+  background: ${() => `url(${staticImage})`};
+  background-position: ${({ isMuted }) =>
+    isMuted ? "-104px -69px" : "-2px -248px"};
+  width: 25px;
+  height: 25px;
+  &:hover {
+    background-position: ${({ isMuted }) =>
+      isMuted ? "-126px -69px" : "-31px -248px"};
+  }
+`;
+
+export const ModeIcon = styled.div<{ mode: ModeProps }>`
+  width: 25px;
+  height: 25px;
+  margin-top: 2px;
+  background: ${() => `url(${staticImage})`};
+  background-position: ${({ mode }) => {
+    if (mode === "single-cycle") {
+      return `-66px -344px`;
+    } else if (mode === "list-cycle") {
+      return "-3px -344px";
+    } else {
+      return `-66px -248px`;
+    }
+  }};
+  &:hover {
+    background-position: ${({ mode }) => {
+      if (mode === "single-cycle") {
+        return `-93px -344px`;
+      } else if (mode === "list-cycle") {
+        return "-33px -344px";
+      } else {
+        return `-93px -248px`;
+      }
+    }};
+  }
+`;
+
+export const PlaylistIcon = styled.div`
+  background: ${() => `url(${staticImage})`};
+  height: 25px;
+  width: 58px;
+  background-position: -42px -68px;
+  padding-left: 18px;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666;
+  text-shadow: 0 1px 0 #080707;
+  &:hover {
+    background-position: -42px -98px;
   }
 `;
