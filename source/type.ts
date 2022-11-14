695f3962e26f5621828e015d26351e3c7cf0882d
tohsaka888 <156132264@qq.com>
2022-11-14 12:00:17 +0800

    ✨ feat: add type declaration

--- source/type.ts
+++ source/type.ts
@@ -1,7 +1,18 @@
 import React from "react";
 
+export type ModeProps = "single-cycle" | "list-cycle" | "random";
+
 export type MusicPlayerProps = {
   name: string | React.ReactNode;
   artist: string | React.ReactNode;
   url: string;
+  autoplay?: boolean;
+  picUrl: string;
+  onPlayNext: () => void;
+  onPlayPrev: () => void;
+  onPictureInPicture: () => void;
+  onCollect: () => void;
+  onShare: () => void;
+  onModeChange: (mode: ModeProps) => void;
+  playlistLength: number;
 };
