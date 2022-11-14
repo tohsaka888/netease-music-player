99f4a03439f7792f3b1b3d108efc85c507f88e9b
tohsaka888 <156132264@qq.com>
2022-11-14 11:28:43 +0800

    ✨ feat: update demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ export default function Home() {
       name={"Beautiful World (Da Capo Version)"}
       artist={"宇多田ヒカル"}
       url={"https://music.163.com/song/media/outer/url?id=1824020873.mp3"}
+      picUrl={
+        "https://p2.music.126.net/l3G4LigZnOxFE9lB4bz_LQ==/109951165791860501.jpg?param=34y34"
+      }
     />
   );
 }
