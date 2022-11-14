5dd3d9d9ddc9d135bc27f797d83be2d0fd35f8c8
tohsaka888 <156132264@qq.com>
2022-11-14 13:50:18 +0800

    ✨ feat: add volume bar

--- source/AddonPanel.tsx
+++ source/AddonPanel.tsx
@@ -1,14 +1,51 @@
 import React, { useState } from "react";
 import { usePlayerProps } from "./controller/PropsController";
-import { ModeIcon, PlaylistIcon, VolumeIcon } from "./index.styled";
+import {
+  ModeIcon,
+  PlaylistIcon,
+  VolumeBarBg,
+  VolumeIcon,
+} from "./index.styled";
 import { ModeProps } from "./type";
+import { motion } from "framer-motion";
+import {
+  useDispatchVolumeVisible,
+  useVolumeVisible,
+} from "./controller/VolumeVisivbleController";
 
 function AddonPanel() {
   const [mode, setMode] = useState<ModeProps>("list-cycle");
   const { onModeChange, playlistLength } = usePlayerProps();
+  const volumeVisible = useVolumeVisible();
+  const dispatchVolumeVisible = useDispatchVolumeVisible();
+
   return (
-    <>
-      <VolumeIcon isMuted={false} />
+    <div
+      style={{ position: "relative", display: "flex", alignItems: "center" }}
+    >
+      {/* volume control */}
+      <motion.div
+        initial={{
+          height: "0px",
+          width: "32px",
+          position: "absolute",
+          bottom: "calc(100% + 10px)",
+          left: "-5px",
+        }}
+        animate={{
+          height: volumeVisible ? "113px" : "0px",
+        }}
+      >
+        <VolumeBarBg />
+      </motion.div>
+
+      <VolumeIcon
+        isMuted={false}
+        onClick={() => {
+          dispatchVolumeVisible(true);
+        }}
+      />
+
       <ModeIcon
         mode={mode}
         onClick={() => {
@@ -25,7 +62,7 @@ function AddonPanel() {
         }}
       />
       <PlaylistIcon>{playlistLength}</PlaylistIcon>
-    </>
+    </div>
   );
 }
 
