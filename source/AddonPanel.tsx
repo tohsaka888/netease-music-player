f329f2b5b4634724754a6beec7a64a674c7e50b3
tohsaka888 <156132264@qq.com>
2022-11-14 11:57:51 +0800

    ✨ feat: add click event

--- source/AddonPanel.tsx
+++ source/AddonPanel.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { usePlayerProps } from "./controller/PropsController";
 import { ModeIcon, PlaylistIcon, VolumeIcon } from "./index.styled";
 import { ModeProps } from "./type";
 
 function AddonPanel() {
   const [mode, setMode] = useState<ModeProps>("list-cycle");
+  const { onModeChange, playlistLength } = usePlayerProps();
   return (
     <>
       <VolumeIcon isMuted={false} />
@@ -12,14 +14,17 @@ function AddonPanel() {
         onClick={() => {
           if (mode === "list-cycle") {
             setMode("single-cycle");
+            onModeChange("single-cycle");
           } else if (mode === "single-cycle") {
             setMode("random");
+            onModeChange("random");
           } else {
             setMode("list-cycle");
+            onModeChange("list-cycle");
           }
         }}
       />
-      <PlaylistIcon>123</PlaylistIcon>
+      <PlaylistIcon>{playlistLength}</PlaylistIcon>
     </>
   );
 }
